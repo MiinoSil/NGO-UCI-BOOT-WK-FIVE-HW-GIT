4371f7db98cda4b5e572f533d5c1fd6b398a26ab
Andrew Ngo <94930359+MiinoSil@users.noreply.github.com>
2022-01-26 15:53:46 -0800

    Working save and remove from localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -38,114 +38,199 @@ $.each(hoursArr, function (index, value) {
     tasksArr = [tasksHour09, tasksHour10, tasksHour11, tasksHour12, tasksHour13, tasksHour14, tasksHour15, tasksHour16, tasksHour17, tasksHour18]
     $(".container").append(
         "<div class='row'><div class='col-2 hour text-center' id='hour'" +
-        (index++) + "'><span>" + value.format("h A") + "</span></div><div class='col-8' id='timeblock" +
-        (index++) + "'><textarea class='col-12' id='taskNm" + (index++) + "'>" + tasksArr[index] + "</textarea></div>" +
-        "<div class='col-2 saveBtn text-center' id='saveBtn'" + (index++) + "'><i class='fas fa-save' title='Save Task'></i> <i class='fas fa-trash' title='Remove Task'></i></div></div></div>");
+        (index) + "'><span>" + value.format("h A") + "</span></div><div class='col-8' id='timeblock" +
+        (index) + "'><textarea class='col-12' id='taskNm" + (index) + "'>" + tasksArr[index] + "</textarea></div>" +
+        "<div class='col-2 saveBtn text-center' id='saveBtn" + (index) + "'><i class='fas fa-save' title='Save Task'></i> <i class='fas fa-trash' title='Remove Task'></i></div></div></div>");
 });
 
+// Function to check each time slot then colorize, change class depending on past, present, or future
 var checkPastPresentFuture = function () {
     currentTime = moment().format("hh:mm:ss");
 
     // For each time block, check time then colorize or change class .past, .present, .future
     if (moment().isBetween(hour09, hour10)) {
-        $("#timeblock1").addClass("present");
+        $("#timeblock0").addClass("present");
     }
     else if (moment().isAfter(hour10)) {
+        $("#timeblock0").addClass("past");
+    }
+    else {
+        $("#timeblock0").addClass("future");
+    };
+
+    if (moment().isBetween(hour10, hour11)) {
+        $("#timeblock1").addClass("present");
+    }
+    else if (moment().isAfter(hour11)) {
         $("#timeblock1").addClass("past");
     }
     else {
         $("#timeblock1").addClass("future");
     };
 
-    if (moment().isBetween(hour10, hour11)) {
+    if (moment().isBetween(hour11, hour12)) {
         $("#timeblock2").addClass("present");
     }
-    else if (moment().isAfter(hour11)) {
+    else if (moment().isAfter(hour12)) {
         $("#timeblock2").addClass("past");
     }
     else {
         $("#timeblock2").addClass("future");
     };
 
-    if (moment().isBetween(hour11, hour12)) {
+    if (moment().isBetween(hour12, hour13)) {
         $("#timeblock3").addClass("present");
     }
-    else if (moment().isAfter(hour12)) {
+    else if (moment().isAfter(hour13)) {
         $("#timeblock3").addClass("past");
     }
     else {
         $("#timeblock3").addClass("future");
     };
 
-    if (moment().isBetween(hour12, hour13)) {
+    if (moment().isBetween(hour13, hour14)) {
         $("#timeblock4").addClass("present");
     }
-    else if (moment().isAfter(hour13)) {
+    else if (moment().isAfter(hour14)) {
         $("#timeblock4").addClass("past");
     }
     else {
         $("#timeblock4").addClass("future");
     };
 
-    if (moment().isBetween(hour13, hour14)) {
+    if (moment().isBetween(hour14, hour15)) {
         $("#timeblock5").addClass("present");
     }
-    else if (moment().isAfter(hour14)) {
+    else if (moment().isAfter(hour15)) {
         $("#timeblock5").addClass("past");
     }
     else {
         $("#timeblock5").addClass("future");
     };
 
-    if (moment().isBetween(hour14, hour15)) {
+    if (moment().isBetween(hour15, hour16)) {
         $("#timeblock6").addClass("present");
     }
-    else if (moment().isAfter(hour15)) {
+    else if (moment().isAfter(hour16)) {
         $("#timeblock6").addClass("past");
     }
     else {
         $("#timeblock6").addClass("future");
     };
 
-    if (moment().isBetween(hour15, hour16)) {
+    if (moment().isBetween(hour16, hour17)) {
         $("#timeblock7").addClass("present");
     }
-    else if (moment().isAfter(hour16)) {
+    else if (moment().isAfter(hour17)) {
         $("#timeblock7").addClass("past");
     }
     else {
         $("#timeblock7").addClass("future");
     };
 
-    if (moment().isBetween(hour16, hour17)) {
+    if (moment().isBetween(hour17, hour18)) {
         $("#timeblock8").addClass("present");
     }
-    else if (moment().isAfter(hour17)) {
+    else if (moment().isAfter(hour18)) {
         $("#timeblock8").addClass("past");
     }
     else {
         $("#timeblock8").addClass("future");
     };
 
-    if (moment().isBetween(hour17, hour18)) {
+    if (moment().isBetween(hour18, hour19)) {
         $("#timeblock9").addClass("present");
     }
-    else if (moment().isAfter(hour18)) {
+    else if (moment().isAfter(hour19)) {
         $("#timeblock9").addClass("past");
     }
     else {
         $("#timeblock9").addClass("future");
-    };
-
-    if (moment().isBetween(hour18, hour19)) {
-        $("#timeblock10").addClass("present");
-    }
-    else if (moment().isAfter(hour19)) {
-        $("#timeblock10").addClass("past");
-    }
-    else {
-        $("#timeblock10").addClass("future");
     };    
 }
+
+// Add event listener for task storage 
+$("#saveBtn0").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour09");
+    $("#taskNm0").val("");
+})
+$("#saveBtn1").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour10");
+    $("#taskNm1").val("");
+})
+$("#saveBtn2").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour11");
+    $("#taskNm2").val("");
+})
+$("#saveBtn3").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour12");
+    $("#taskNm3").val("");
+})
+$("#saveBtn4").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour13");
+    $("#taskNm4").val("");
+})
+$("#saveBtn5").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour14");
+    $("#taskNm5").val("");
+})
+$("#saveBtn6").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour15");
+    $("#taskNm6").val("");
+})
+$("#saveBtn7").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour16");
+    $("#taskNm7").val("");
+})
+$("#saveBtn8").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour17");
+    $("#taskNm8").val("");
+})
+$("#saveBtn9").on("click", "i.fa-trash", function () {
+    localStorage.removeItem("hour18");
+    $("#taskNm9").val("");
+})
+
+// Add event listener for task removal
+$("#saveBtn0").on("click", "i.fa-save", function () {
+    var task0 = $("#taskNm0").val().trim();
+    localStorage.setItem('hour09', JSON.stringify(task0));
+})
+$("#saveBtn1").on("click", "i.fa-save", function () {
+    var task1 = $("#taskNm1").val().trim();
+    localStorage.setItem('hour10', JSON.stringify(task1));
+})
+$("#saveBtn2").on("click", "i.fa-save", function () {
+    var task2 = $("#taskNm2").val().trim();
+    localStorage.setItem('hour11', JSON.stringify(task2));
+})
+$("#saveBtn3").on("click", "i.fa-save", function () {
+    var task3 = $("#taskNm3").val().trim();
+    localStorage.setItem('hour12', JSON.stringify(task3));
+})
+$("#saveBtn4").on("click", "i.fa-save", function () {
+    var task4 = $("#taskNm4").val().trim();
+    localStorage.setItem('hour13', JSON.stringify(task4));
+})
+$("#saveBtn5").on("click", "i.fa-save", function () {
+    var task5 = $("#taskNm5").val().trim();
+    localStorage.setItem('hour14', JSON.stringify(task5));
+})
+$("#saveBtn6").on("click", "i.fa-save", function () {
+    var task6 = $("#taskNm6").val().trim();
+    localStorage.setItem('hour15', JSON.stringify(task6));
+})
+$("#saveBtn7").on("click", "i.fa-save", function () {
+    var task7 = $("#taskNm7").val().trim();
+    localStorage.setItem('hour16', JSON.stringify(task7));
+})
+$("#saveBtn8").on("click", "i.fa-save", function () {
+    var task8 = $("#taskNm8").val().trim();
+    localStorage.setItem('hour17', JSON.stringify(task8));
+})
+$("#saveBtn9").on("click", "i.fa-save", function () {
+    var task9 = $("#taskNm9").val().trim();
+    localStorage.setItem('hour18', JSON.stringify(task9));
+})
 
 checkPastPresentFuture();
